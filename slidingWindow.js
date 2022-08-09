8c1dfbf2cc6b7213e7382be1d5c90e79418d88cf
VINAY CHANDRAN C <100291822+vinayjs@users.noreply.github.com>
2022-08-09 22:08:40 +0530

    Add files via upload

--- slidingWindow.js
+++ slidingWindow.js
@@ -6,7 +6,6 @@
 //   }
 //   return result;
 // };
-
 // const findMax = (index, size, array) => {
 //   const result = [];
 //   for (let i = 0; i < size; i++) {
@@ -115,25 +114,120 @@
 // Output: 1
 // Explanation: The smallest subarray with a sum greater than or equal to ‘7’ is [8].
 
-const smallSubArray = (s, arr) => {
-  let minLength = Infinity;
-  let windowSum = 0;
-  let windowStart = 0;
+// const smallSubArray = (s, arr) => {
+//   let minLength = Infinity;
+//   let windowSum = 0;
+//   let windowStart = 0;
 
-  for(let i = 0; i < arr.length; i++) {
-    windowSum += arr[i];
+//   for(let i = 0; i < arr.length; i++) {
+//     windowSum += arr[i];
 
-    while(windowSum >= s) {
-      minLength = Math.min(minLength, i - windowStart + 1);
-      windowSum -= arr[windowStart];
-      windowStart += 1
-    }
-  }
+//     while(windowSum >= s) {
+//       minLength = Math.min(minLength, i - windowStart + 1);
+//       windowSum -= arr[windowStart];
+//       windowStart += 1
+//     }
+//   }
 
-  if(minLength === Infinity) {
-    return 0;
-  }
-  return minLength;
-}
+//   if(minLength === Infinity) {
+//     return 0;
+//   }
+//   return minLength;
+// }
 
-console.log(smallSubArray(7, [2, 1, 5, 2, 3, 2]));
+// console.log(smallSubArray(7, [2, 1, 5, 2, 3, 2]));
+
+// Given a string, find the length of the longest substring in it with no more than K distinct characters.
+// Input: String="araaci", K=2
+// Output: 4
+// Explanation: The longest substring with no more than '2' distinct characters is "araa".
+
+// const subString = (str, k) => {
+//   let windowStart = 0,
+//   maxLength = 0,
+//   charFrequency = {};
+// // in the following loop we'll try to extend the range [window_start, window_end]
+// for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
+//   const rightChar = str[windowEnd];
+//   if (!(rightChar in charFrequency)) {
+//     charFrequency[rightChar] = 0;
+//   }
+//   charFrequency[rightChar] += 1;
+//   // shrink the sliding window, until we are left with 'k' distinct characters in the char_frequency
+//   while (Object.keys(charFrequency).length > k) {
+//     const leftChar = str[windowStart];
+//     charFrequency[leftChar] -= 1;
+//     if (charFrequency[leftChar] === 0) {
+//       delete charFrequency[leftChar];
+//     }
+//     windowStart += 1; // shrink the window
+//   }
+//   // remember the maximum length so far
+//   maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+// }
+
+// return maxLength;
+// }
+
+// console.log(subString("araaci", 2));
+//=============================================================================================================================
+
+
+// Input: Fruit=['A', 'B', 'C', 'A', 'C']
+// Output: 3
+// Explanation: We can put 2 'C' in one basket and one 'A' in the other from the subarray ['C', 'A', 'C']
+
+// const fruits_into_baskets = (fruits) => {
+//   let windowStart = 0,
+//     maxLength = 0,
+//     fruitFrequency = {};
+
+//   // try to extend the range [windowStart, windowEnd]
+//   for (let windowEnd = 0; windowEnd < fruits.length; windowEnd++) {
+//     const rightFruit = fruits[windowEnd];
+//     if (!(rightFruit in fruitFrequency)) {
+//       fruitFrequency[rightFruit] = 0;
+//     }
+//     fruitFrequency[rightFruit] += 1;
+
+//     // shrink the sliding window, until we are left with '2' fruits in the fruit frequency dictionary
+//     while (Object.keys(fruitFrequency).length > 2) {
+//       const leftFruit = fruits[windowStart];
+//       fruitFrequency[leftFruit] -= 1;
+//       if (fruitFrequency[leftFruit] === 0) {
+//         delete fruitFrequency[leftFruit];
+//       }
+//       windowStart += 1; // shrink the window
+//     }
+//     maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+//   }
+
+//   return maxLength;
+// }
+
+// console.log(fruits_into_baskets(['A', 'B', 'C', 'A', 'C']));
+
+
+//============================================================================================================================
+// Given a string, find the length of the longest substring, which has all distinct characters.
+
+// Input: String="aabccbb"
+// Output: 3
+// Explanation: The longest substring with distinct characters is "abc".
+
+// const non_repeat_substring = (str) => {
+//   let windowStart = 0,
+//     maxLength = 0,
+//     charIndexMap = {};
+//   for (let windowEnd = 0; windowEnd < str.length; windowEnd++) {
+//     const rightChar = str[windowEnd];
+//     if (rightChar in charIndexMap) {
+//       windowStart = Math.max(windowStart, charIndexMap[rightChar] + 1);
+//     }
+//     charIndexMap[rightChar] = windowEnd;
+//     maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+//   }
+//   return maxLength;
+// }
+
+// console.log(non_repeat_substring("aabccbb"))
