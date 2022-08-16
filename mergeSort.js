cfbfd32f5a92f07c8fde7e30e3e3a8dacea3fec3
VINAY CHANDRAN C <100291822+vinayjs@users.noreply.github.com>
2022-08-16 20:44:54 +0530

    Add files via upload

--- mergeSort.js
+++ mergeSort.js
@@ -1,3 +1,7 @@
+// Given a list of intervals, 
+// merge all the overlapping intervals to produce
+// a list that has only mutually exclusive intervals.
+
 // Intervals: [[1,4], [2,5], [7,9]]
 // Output: [[1,5], [7,9]]
 // Explanation: Since the first two intervals [1,4] and [2,5] overlap, we merged them into
