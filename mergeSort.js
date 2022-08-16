ebe990dfa16f26e968eb7b8e9d65db2c0a79821a
VINAY CHANDRAN C <100291822+vinayjs@users.noreply.github.com>
2022-08-16 20:40:50 +0530

    Add files via upload

--- mergeSort.js
+++ mergeSort.js
@@ -9,7 +9,7 @@
 //     this.end = end;
 //   }
 
-//   get_interval() {
+//   getInterval() {
 //     return "[" + this.start + ", " + this.end + "]";
 //   }
 // }
@@ -42,3 +42,52 @@
 //   new Interval(7, 9),
 // ]);
 // console.log("Merged intervals :", result);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Given a list of non-overlapping intervals sorted by their start time,
+// insert a given interval at the correct position and
+// merge all necessary intervals to produce a list
+// that has only mutually exclusive intervals.
+
+// Input: Intervals=[[1,3], [5,7], [8,12]], New Interval=[4,6]
+// Output: [[1,3], [4,7], [8,12]]
+// Explanation: After insertion, since [4,6] overlaps with [5,7],
+// we merged them into one [4,7].
+
+// class Interval {
+//   constructor(start, end) {
+//     this.start = start;
+//     this.end = end;
+//   }
+//   printInterval() {
+//     return "[" + this.start + ", " + this.end + "]";
+//   }
+// }
+
+// const insert = (intervals, newInterval) => {
+//   let merged = [];
+//   i = 0;
+//   while (i < intervals.length && intervals[i].end < newInterval.start) {
+//     merged.push(intervals[i]);
+//     i += 1;
+//   }
+//   while (i < intervals.length && intervals[i].start <= newInterval.end) {
+//     newInterval.start = Math.min(intervals[i].start, newInterval.start);
+//     newInterval.end = Math.max(intervals[i].end, newInterval.end);
+//     i += 1;
+//   }
+//   merged.push(newInterval);
+
+//   while (i < intervals.length) {
+//     merged.push(intervals[i]);
+//     i += 1;
+//   }
+//   return merged;
+// };
+// let result = insert(
+//   [new Interval(1, 3), new Interval(5, 7), new Interval(8, 12)],
+//   new Interval(4, 6)
+// );
+
+// console.log("new output :", result);
