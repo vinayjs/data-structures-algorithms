b853f675cbf4fa3b6e27257e81a8f3e3b50f4300
VINAY CHANDRAN C <100291822+vinayjs@users.noreply.github.com>
2022-08-16 11:24:22 +0530

    Add files via upload

--- slidingWindow.js
+++ slidingWindow.js
@@ -385,3 +385,54 @@
 //   return str.substring(substrStart, substrStart + minLength);
 // };
 // console.log(findSubstring("aabdec", "abc"));
+
+// Given a string and a list of words,
+// find all the starting indices of substrings in the given string
+// that are a concatenation of all the given words exactly once without any overlapping of words.
+// It is given that all words are of the same length.
+
+// Input: String="catfoxcat", Words=["cat", "fox"]
+// Output: [0, 3]
+// Explanation: The two substring containing both the words are "catfox" & "foxcat".
+
+// Input: String="catcatfoxfox", Words=["cat", "fox"]
+// Output: [3]
+// Explanation: The only substring containing both the words is "catfox".
+
+// const findWordConcatenation = (str, words) => {
+//   if (words.length === 0 || words[0].length === 0) {
+//     return [];
+//   }
+//   wordFrequency = {};
+//   words.forEach((word) => {
+//     if (!(word in wordFrequency)) {
+//       wordFrequency[word] = 0;
+//     }
+//     wordFrequency[word] += 1;
+//   });
+//   const resultIndices = [],
+//     wordsCount = words.length;
+//     wordLength = words[0].length;
+//     for (i = 0; i < str.length - wordsCount * wordLength + 1; i++) {
+//          const wordsSeen = {};
+//         for (j = 0; j < wordsCount; j++) {
+//             next_word_index = i + j * wordLength;
+//             word = str.substring(next_word_index, next_word_index + wordLength);
+//             if (!(word in wordFrequency)) {
+//                  break;
+//             }
+//             if (!(word in wordsSeen)) {
+//                  wordsSeen[word] = 0;
+//             }
+//             wordsSeen[word] += 1;
+//             if (wordsSeen[word] > (wordFrequency[word] || 0)) {
+//              break;
+//             }
+//              if (j + 1 === wordsCount) {
+//             resultIndices.push(i);
+//              }
+//         }
+//   }
+//   return resultIndices;
+// };
+// console.log(findWordConcatenation("catfoxcat", ["cat", "fox"]));
