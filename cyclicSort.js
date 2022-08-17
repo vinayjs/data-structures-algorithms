9aa2f49efd32b84f87b98ded5e0d2f9135dfb25c
VINAY CHANDRAN C <100291822+vinayjs@users.noreply.github.com>
2022-08-17 22:06:37 +0530

    Add files via upload

--- cyclicSort.js
+++ cyclicSort.js
@@ -115,3 +115,34 @@
 //     return -1;
 // }
 // console.log(findDupllicate([2, 1, 3, 3, 5, 4]));
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// We are given an unsorted array containing n numbers taken from the range 1 to n. The array has some numbers appearing twice, 
+// find all these duplicate numbers using constant space.
+
+// Input: [3, 4, 4, 5, 5]
+// Output: [4, 5]
+
+// Input: [5, 4, 7, 2, 3, 5, 3]
+// Output: [3, 5]
+
+// const findAllDups = (nums) => {
+//     let i = 0;
+//     while (i < nums.length) {
+//         j = nums[i] - 1;
+//         if (nums[i] !== nums[j]) {
+//             [nums[i], nums[j]] = [nums[j], nums[i]];
+//         } else {
+//             i++;
+//         }
+//     }
+//     duplicateNumbers = [];
+//     for (let i = 0; i < nums.length; i++) {
+//         if (nums[i] !== i + 1) {
+//             duplicateNumbers.push(nums[i]);
+//         }
+//     }
+//     return duplicateNumbers;
+// }
+
+// console.log(findAllDups([5, 4, 7, 2, 3, 5, 3]));
