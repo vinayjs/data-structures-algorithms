7ed00fe03f12b762fdd0192bdb34c506ac05e8c8
VINAY CHANDRAN C <100291822+vinayjs@users.noreply.github.com>
2022-08-17 18:07:19 +0530

    Add files via upload

--- inPlaceReversalLinkList.js
+++ inPlaceReversalLinkList.js
@@ -241,3 +241,56 @@
 // result.printList();
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Given the head of a Singly LinkedList and a number ‘k’,
+// rotate the LinkedList to the right by ‘k’ nodes.
+
+// class Node {
+//     constructor(value, next = null) {
+//       this.value = value;
+//       this.next = next;
+//     }
+//     printList() {
+//       let temp = this;
+//       while (temp != null) {
+//         console.log(`${temp.value}`);
+//         temp = temp.next;
+//       }
+//       console.log();
+//     }
+//   }
+
+// const rotate = (head, rotations) => {
+//     if (head === null || head.next === null || rotations <= 0){
+//         return head;
+//     }
+//     let lastNode = head;
+//     let listLength = 1;
+//     while (lastNode.next !== null) {
+//         lastNode = lastNode.next;
+//         listLength += 1;
+//     }
+//     lastNode.next = head;
+//     rotations %= listLength;
+//     skipLength = listLength - rotations;
+//     lastNodeOfRotatedList = head;
+//     for (i = 0; i < skipLength - 1; i++) {
+//         lastNodeOfRotatedList = lastNodeOfRotatedList.next;
+//     }
+//     head = lastNodeOfRotatedList.next;
+//     lastNodeOfRotatedList.next = null;
+//     return head;
+// }
+// const head = new Node(1);
+// head.next = new Node(2);
+// head.next.next = new Node(3);
+// head.next.next.next = new Node(4);
+// head.next.next.next.next = new Node(5);
+// head.next.next.next.next.next = new Node(6);
+// head.next.next.next.next.next.next = new Node(7);
+// head.next.next.next.next.next.next.next = new Node(8);
+
+// console.log("previous node : ");
+// head.printList();
+// result = rotate(head, 3);
+// console.log("new order : ");
+// result.printList();
