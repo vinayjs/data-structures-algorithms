c2cc7540ff4e087d4325deb2afdb6b9ad4e4e818
VINAY CHANDRAN C <100291822+vinayjs@users.noreply.github.com>
2022-08-17 15:51:14 +0530

    Add files via upload

--- inPlaceReversalLinkList.js
+++ inPlaceReversalLinkList.js
@@ -102,3 +102,70 @@
 // result = reverseSubList(head, 3, 5);
 // console.log("new order : ");
 // result.printList();
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Given the head of a LinkedList and a number ‘k’, reverse every ‘k’ sized sub-list starting from the head.
+
+// If, in the end, you are left with a sub-list with less than ‘k’ elements, reverse it too.
+
+// class Node {
+//   constructor(value, next = null) {
+//     this.value = value;
+//     this.next = next;
+//   }
+//   printList() {
+//     let temp = this;
+//     while (temp != null) {
+//       console.log(`${temp.value}`);
+//       temp = temp.next;
+//     }
+//     console.log();
+//   }
+// }
+
+// const reverseElements = (head, k) => {
+//   if (k <= 1 || head === null) {
+//     return head;
+//   }
+//   let current = head,
+//     previous = null;
+//   while (true) {
+//     const lastNodePrevPart = previous;
+//     const lastNodeSubList = current;
+//     let next = null;
+//     let i = 0;
+//     while (current !== null && i < k) {
+//       next = current.next;
+//       current.next = previous;
+//       previous = current;
+//       current = next;
+//       i += 1;
+//     }
+//     if (lastNodePrevPart !== null) {
+//       lastNodePrevPart.next = previous;
+//     } else {
+//       head = previous;
+//     }
+//     lastNodeSubList.next = current;
+//     if (current === null) {
+//       break;
+//     }
+//     previous = lastNodeSubList;
+//   }
+//   return head;
+// };
+// const head = new Node(1);
+// head.next = new Node(2);
+// head.next.next = new Node(3);
+// head.next.next.next = new Node(4);
+// head.next.next.next.next = new Node(5);
+// head.next.next.next.next.next = new Node(6);
+// head.next.next.next.next.next.next = new Node(7);
+// head.next.next.next.next.next.next.next = new Node(8);
+
+// console.log("previous node : ");
+// head.printList();
+// result = reverseElements(head, 3);
+// console.log("new order : ");
+// result.printList();
