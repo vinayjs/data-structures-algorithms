65731712ea7ab4bf16b33f013ba8d3f6c3ad84c8
VINAY CHANDRAN C <100291822+vinayjs@users.noreply.github.com>
2022-08-17 20:15:54 +0530

    Add files via upload

--- cyclicSort.js
+++ cyclicSort.js
@@ -86,3 +86,32 @@
 //   return missingNumbers;
 // };
 // console.log(findMissingNum([2, 3, 1, 8, 2, 3, 5, 1]));
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// We are given an unsorted array containing ‘n+1’ numbers taken from the range 1 to ‘n’. 
+// The array has only one duplicate but it can be repeated multiple times. Find that duplicate number without using any extra space. 
+// You are, however, allowed to modify the input array.
+
+// Input: [1, 4, 4, 3, 2]
+// Output: 4
+
+// Input: [2, 1, 3, 3, 5, 4]
+// Output: 3
+
+// const findDupllicate = (nums) => {
+//     let i = 0;
+//     while (i < nums.length) {
+//         if (nums[i] !== i + 1) {
+//             j = nums[i] - 1;
+//             if (nums[i] !== nums[j]) {
+//                 [nums[i],nums[j]] = [nums[j], nums[i]];
+//             } else {
+//                 return nums[i];
+//             }
+//         } else {
+//             i += 1;
+//         }
+//     }
+//     return -1;
+// }
+// console.log(findDupllicate([2, 1, 3, 3, 5, 4]));
